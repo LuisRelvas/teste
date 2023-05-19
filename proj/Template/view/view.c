2457cfa3b9cbd66bbbcc7e805b78e9214f7fd83b
LuisRelvas <luisrelvas@netcabo.pt>
2023-05-19 12:13:30 +0100

    Otimizing

--- proj/Template/view/view.c
+++ proj/Template/view/view.c
@@ -231,6 +231,8 @@ void draw_mode_menu()
 Sprite **matrix;
 Sprite *index_1;
 Sprite *index_2;
+bool *index_1_bool;
+bool *index_2_bool;
 
 
 int id1 = 0; 
@@ -457,10 +459,11 @@ bool check_match(int id1, int id2)
 
 void process_button1(Sprite *cards, int size)
 {
-    printf("process_button1\n");
-    if (button1->pressed)
+    if (mouse_info.x >= 0 && mouse_info.y >= 0 && mouse_info.x <= mode_info.XResolution / 2 && mouse_info.y <= mode_info.XResolution / 2)
     {
+        if(mouse_info.left_click){
         button1Pressed = true;
+        
         counter_cards++;
         if(matrix_id1 == -1){
             matrix_id1 = matrix[0][0].id;
@@ -468,41 +471,50 @@ void process_button1(Sprite *cards, int size)
         else if(matrix_id2 == -1) {
             matrix_id2 = matrix[0][0].id;
          }
+        }
         
     }
         if (button1Pressed)
     {
         if(index_1 == NULL){
+        index_1_bool = &button1Pressed;
+       
         index_1 = &matrix[0][0];
-        printf("The value of index_1 is: %d\n", index_1); 
+        
         }
         else if(index_2 == NULL) {
+            index_2_bool = &button1Pressed;
+            
             index_2 = &matrix[0][0];
-            printf("The value of index_2 is: %d\n", index_2);
+           
             
         }
-        printf("matrix bool: %d\n", matrix[0][0].block);
         draw_sprite_xpm(&matrix[0][0], 0, 0);
         if(matrix_id1 != -1 && matrix_id2 != -1) {
-        if (check_match(matrix_id1,matrix_id2))
-        printf("Match found");
+        if (check_match(matrix_id1,matrix_id2)){
+            printf("MATCH FOUND");
         index_1->block = true;
-        index_2->block = true;}
-         }
-         else{
+        index_2->block = true;
+        }
+
             index_1 = NULL;
             index_2 = NULL;
             matrix_id1 = -1;
             matrix_id2 = -1;
+            index_1_bool = false;
+            index_2_bool = false;
+            
             
          }
         
+    }
 }
 
 void process_button2(Sprite *cards, int size)
 {
-    if (button2->pressed)
+    if (mouse_info.x >= 0 && mouse_info.y >= 0 && mouse_info.x <= mode_info.XResolution && mouse_info.y <= mode_info.XResolution / 2)
     {
+        if(mouse_info.left_click){
         button2Pressed = true;
         counter_cards++;
         if(matrix_id1 == 0) { 
@@ -511,272 +523,137 @@ void process_button2(Sprite *cards, int size)
         else if(matrix_id2 != 0) { 
             matrix_id2 = matrix[0][1].id;
         }
+        }
     }
 
     if (button2Pressed)
     {if(index_1 == NULL){
-        index_1 = &matrix[0][1]; 
-
-        printf("The value of index_1 is: %d\n", index_1);}
+        index_1_bool = &button2Pressed;
+        index_1 = &matrix[0][1]; }
         else if(index_2 == NULL) {
+            index_2_bool = &button2Pressed;
             
             index_2 = &matrix[0][1];
-            printf("The value of index_2 is: %d\n", index_2);
-            
         }
-        printf("matrix bool: %d\n", matrix[0][0].block);
         draw_sprite_xpm(&matrix[0][1], mode_info.XResolution / 2, 0);
         if(matrix_id1 != -1 && matrix_id2 != -1) {
-        if (check_match(matrix_id1,matrix_id2))
-        printf("Match found");
+        if (check_match(matrix_id1,matrix_id2)){
+        printf("MATCH FOUND");
         index_1->block = true;
         index_2->block = true;
         }
-        else {
+
             index_1 = NULL;
             index_2 = NULL;
             matrix_id1 = -1;
             matrix_id2 = -1;
-
-            
-        }
-        
+            index_1_bool = false;
+            index_2_bool = false;
     }
+}
 }
 
 void process_button3(Sprite *cards, int size)
 {
-    if (button3->pressed)
+    if (mouse_info.x >= 0 && mouse_info.y >= mode_info.YResolution / 2 && mouse_info.x <= mode_info.XResolution / 2 && mouse_info.y <= mode_info.XResolution)
     {
+        if(mouse_info.left_click){
         button3Pressed = true;
+        
         counter_cards++;
+        if(matrix_id1 == -1){
+            matrix_id1 = matrix[1][0].id;
+        } 
+        else if(matrix_id2 == -1) {
+            matrix_id2 = matrix[1][0].id;
+         }
+        }
+        
     }
-
-    if (button3Pressed)
+        if (button3Pressed)
     {
+        if(index_1 == NULL){
+        index_1_bool = &button3Pressed;
+       
+        index_1 = &matrix[1][0];
+        
+        }
+        else if(index_2 == NULL) {
+            index_2_bool = &button3Pressed;
+            
+            index_2 = &matrix[1][0];
+           
+            
+        }
         draw_sprite_xpm(&matrix[1][0], 0, mode_info.YResolution / 2);
-        if (button1Pressed)
-        {
-            if (check_match(matrix[0][0].id, matrix[1][0].id))
-            { // match entre o id 1 e o id 2;
-                block1 = true;
-                block3 = true;
-                button1Pressed = false;
-                button3Pressed = false;
-                final++;
-                if(menuState == GAME_4_2) {
-                if(player1){
-                    player_1++;
-                }
-                else if(player2){
-                    player_2++;
-                }
-                }
-            }
-            else
-            {
-                button1Pressed = false;
-                button3Pressed = false;
-                counter_cards = 0;
-                if(menuState == GAME_4_2) { 
-                if(player1) { 
-                    player1 = false; 
-                    player2 = true;
-                }
-                else if(player2) { 
-                    player2 = false; 
-                    player1 = true;
-                }
-                }
-            }
+        if(matrix_id1 != -1 && matrix_id2 != -1) {
+        if (check_match(matrix_id1,matrix_id2)){
+        index_1->block = true;
+        index_2->block = true;
         }
-        if (button2Pressed)
-        {
-            if (check_match(matrix[0][1].id, matrix[1][0].id))
-            {
-                block2 = true;
-                block3 = true;
-                button2Pressed = false;
-                button3Pressed = false;
-                final++;
-                if(menuState == GAME_4_2) {
-                if(player1){
-                    player_1++;
-                }
-                else if(player2){
-                    player_2++;
-                }
-                }
-            }
-            else
-            {
-                button2Pressed = false;
-                button3Pressed = false;
-                counter_cards = 0;
-                if(menuState == GAME_4_2) { 
-                if(player1) { 
-                    player1 = false; 
-                    player2 = true;
-                }
-                else if(player2) { 
-                    player2 = false; 
-                    player1 = true;
-                }
-                }
-            }
-        }
-        if (button4Pressed)
-        {
-            if (check_match(matrix[1][0].id, matrix[1][1].id))
-            {
-                block3 = true;
-                block4 = true;
-                final++;
-                if(menuState == GAME_4_2) {
-                if(player1){
-                    player_1++;
-                }
-                else if(player2){
-                    player_2++;
-                }
-                }
-            }
 
-            else
-            {
-                button3Pressed = false;
-                button4Pressed = false;
-                counter_cards = 0;
-                if(menuState == GAME_4_2) { 
-                if(player1) { 
-                    player1 = false; 
-                    player2 = true;
-                }
-                else if(player2) { 
-                    player2 = false; 
-                    player1 = true;
-                }
-                }
-            }
-        }
+            index_1 = NULL;
+            index_2 = NULL;
+            matrix_id1 = -1;
+            matrix_id2 = -1;
+            index_1_bool = false;
+            index_2_bool = false;
+            
+            
+         }
+        
     }
 }
 
 void process_button4(Sprite *cards, int size)
 {
-    if (button4->pressed)
+   if (mouse_info.x >= mode_info.XResolution / 2 && mouse_info.y >= mode_info.YResolution / 2 && mouse_info.x <= mode_info.XResolution && mouse_info.y <= mode_info.XResolution)
     {
+        if(mouse_info.left_click){
         button4Pressed = true;
+        
         counter_cards++;
+        if(matrix_id1 == -1){
+            matrix_id1 = matrix[1][1].id;
+        } 
+        else if(matrix_id2 == -1) {
+            matrix_id2 = matrix[1][1].id;
+         }
+        }
+        
     }
-
-    if (button4Pressed)
+        if (button4Pressed)
     {
-        draw_sprite_xpm(&matrix[1][1], mode_info.XResolution / 2, mode_info.YResolution / 2);
-        if (button1Pressed)
-        {
-            if (check_match(matrix[0][0].id, matrix[1][1].id))
-            { // match entre o id 1 e o id 2;
-                block1 = true;
-                block4 = true;
-                button1Pressed = false;
-                button4Pressed = false;
-                final++;
-                if(menuState == GAME_4_2) {
-                if(player1){
-                    player_1++;
-                }
-                else if(player2){
-                    player_2++;
-                }
-                }
-            }
-            else
-            {
-                button1Pressed = false;
-                button4Pressed = false;
-                counter_cards = 0;
-                if(menuState == GAME_4_2) { 
-                if(player1) { 
-                    player1 = false; 
-                    player2 = true;
-                }
-                else if(player2) { 
-                    player2 = false; 
-                    player1 = true;
-                }
-                }
-            }
+        if(index_1 == NULL){
+        index_1_bool = &button4Pressed;
+       
+        index_1 = &matrix[1][1];
+        
         }
-        if (button2Pressed)
-        {
-            if (check_match(matrix[0][1].id, matrix[1][1].id))
-            {
-                block2 = true;
-                block4 = true;
-                button2Pressed = false;
-                button4Pressed = false;
-                final++;
-                if(menuState == GAME_4_2) {
-                if(player1){
-                    player_1++;
-                }
-                else if(player2){
-                    player_2++;
-                }
-                }
-            }
-            else
-            {
-                button2Pressed = false;
-                button4Pressed = false;
-                counter_cards = 0;
-                if(menuState == GAME_4_2) { 
-                if(player1) { 
-                    player1 = false; 
-                    player2 = true;
-                }
-                else if(player2) { 
-                    player2 = false; 
-                    player1 = true;
-                }
-                }
-            }
+        else if(index_2 == NULL) {
+            index_2_bool = &button4Pressed;
+            
+            index_2 = &matrix[1][1];
+           
+            
         }
-        if (button3Pressed)
-        {
-            if (check_match(matrix[1][0].id, matrix[1][1].id))
-            {
-                block3 = true;
-                block4 = true;
-                button3Pressed = false;
-                button4Pressed = false;
-                if(menuState == GAME_4_2) {
-                if(player1){
-                    player_1++;
-                }
-                else if(player2){
-                    player_2++;
-                }
-                }
-            }
-            else
-            {
-                button3Pressed = false;
-                button4Pressed = false;
-                counter_cards = 0;
-                if(menuState == GAME_4_2) { 
-                if(player1) { 
-                    player1 = false; 
-                    player2 = true;
-                }
-                else if(player2) { 
-                    player2 = false; 
-                    player1 = true;
-                }
-                }
-            }
+        draw_sprite_xpm(&matrix[1][1], mode_info.XResolution / 2, mode_info.YResolution/2);
+        if(matrix_id1 != -1 && matrix_id2 != -1) {
+        if (check_match(matrix_id1,matrix_id2)){
+        index_1->block = true;
+        index_2->block = true;
         }
+
+            index_1 = NULL;
+            index_2 = NULL;
+            matrix_id1 = -1;
+            matrix_id2 = -1;
+            index_1_bool = false;
+            index_2_bool = false;
+            
+            
+         }
+        
     }
 }
 
@@ -789,6 +666,9 @@ void draw_game_menu()
     Sprite *back2 = back;
     Sprite *back3 = back;
     Sprite *back4 = back;
+
+   
+    
     Sprite cards[] = {*number1, *number1, *number2, *number2};
     int j = sizeof(cards) / sizeof(cards[0]);
 
@@ -800,9 +680,6 @@ void draw_game_menu()
         printf("matrix allocated\n,%d",matrix);
         pre++;
     }
-    
-    
-    
     
     memset(drawing_frame_buffer, 0, mode_info.XResolution * mode_info.YResolution * mode_info.BitsPerPixel / 8);
     draw_sprite_xpm(back1, 0, 0);
@@ -819,37 +696,38 @@ void draw_game_menu()
     }
     if(matrix[0][0].block == true) 
     { 
+        printf("CONSEGUIU BLOQUEAR");
         draw_sprite_xpm(&matrix[0][0], 0, 0);
     }
-    
-    if (!block2)
+
+    if (matrix[0][1].block == false)
     {
         process_button2(cards, j);
-        printf("player1: %d\n", player1);
-        printf("player2: %d\n", player2);
+    
     }
-    else if (block2)
+    else if (matrix[0][1].block == true)
     {
+        printf("CONSEGUIU BLOQUEAR");
         draw_sprite_xpm(&matrix[0][1], mode_info.XResolution / 2, 0);
-        printf("player1: %d\n", player1);
-        printf("player2: %d\n", player2);
+     
     }
-    if (!block3)
+    if (matrix[1][0].block == false)
     {
         process_button3(cards, j);
-        printf("player1: %d\n", player1);
-        printf("player2: %d\n", player2);
+     
     }
-    else if (block3)
+    else if (matrix[1][0].block == true)
     {
+        printf("CONSEGUIU BLOQUEAR");
         draw_sprite_xpm(&matrix[1][0], 0, mode_info.YResolution / 2);
     }
-    if (!block4)
+    if (matrix[1][1].block == false)
     {
         process_button4(cards, j);
     }
-    else if (block4)
+    else if (matrix[1][1].block == true)
     {
+        printf("CONSEGUIU BLOQUEAR");
         draw_sprite_xpm(&matrix[1][1], mode_info.XResolution / 2, mode_info.YResolution / 2);
     }
     if (block1 && block2 && block3 && block4)
@@ -863,6 +741,7 @@ void draw_game_menu()
 
 void draw_game_menu_game_2() 
 {
+    
     Sprite *back1 = back;
     Sprite *back2 = back;
     Sprite *back3 = back;
@@ -885,8 +764,7 @@ void draw_game_menu_game_2()
     if (!block1)
     {
         process_button1(cards, j);
-        printf("player1: %d\n", player1);
-        printf("player2: %d\n", player2);
+     
        
     }
     else if (block1)
@@ -7916,6 +7794,7 @@ void draw_game_menu_game_4() {
 
 
 }
+
 
 // O menu final é apenas um retângulo com tamanho máximo, com um smile ao centro
 void draw_finish_menu()
